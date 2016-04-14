a6e74da095dbcbd8692adfd7656d27727842b134
smlc <siesa.marlon@hotmail.fr>
2016-04-14 16:40:10 +0200

    init

---
+++ src/main/java/examen/dcll/App.java
@@ -0,0 +1,23 @@
+package examen.dcll;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+    /**
+     * Constructor.
+     */
+    protected App() {
+
+    }
+
+
+    /**
+     *
+     *  @param args argument
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/examen/dcll/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by mars on 14/04/16.
+ */
+package examen.dcll;
